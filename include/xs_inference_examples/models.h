41b3d6c17730e8f8ba5364f4e5dc78715ba754af
akrozhin <akrozhin@miem.hse.ru>
2023-09-14 17:38:59 +0300

    impl vanilla structure

---
+++ include/xs_inference_examples/models.h
@@ -0,0 +1,9 @@
+//
+// Created by rozhin on 14.09.2023.
+// Copyright (c) 2021-2023 xsdnn. All rights reserved.
+//
+
+#ifndef XS_INFERENCE_EXAMPLES_MODELS_H
+#define XS_INFERENCE_EXAMPLES_MODELS_H
+
+#endif //XS_INFERENCE_EXAMPLES_MODELS_H
---
+++ src/label_image.cc
@@ -0,0 +1,4 @@
+//
+// Created by rozhin on 14.09.2023.
+// Copyright (c) 2021-2023 xsdnn. All rights reserved.
+//
